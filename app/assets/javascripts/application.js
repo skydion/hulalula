3a0ca8d027f26606d8a101473e35dfc367f3435a
Taras Brodovych <skydion@ukr.net>
2014-08-30 22:51:37 +0300

    fixed expand button, footer and some js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,61 +17,66 @@
 //= require_tree .
 
 /*
-$(document).ready(function() {
-    // Just for demonstration purposes, change the contents/active state using jQuery
-    $("#menu ul li a").click(function () {
-        $("#menu ul li a").each(function () {
-            $(this).removeClass("active");
-        });
+ $(document).ready(function() {
+ // Just for demonstration purposes, change the contents/active state using jQuery
+ $("#menu ul li a").click(function () {
+ $("#menu ul li a").each(function () {
+ $(this).removeClass("active");
+ });
 
-        $(this).addClass("active");
-    });
-});
-*/
+ $(this).addClass("active");
+ });
+ });
+ */
 
 /*function getScrollBarWidth () {
-    var inner = document.createElement('p');
-    inner.style.width = "100%";
-    inner.style.height = "200px";
+ var inner = document.createElement('p');
+ inner.style.width = "100%";
+ inner.style.height = "200px";
 
-    var outer = document.createElement('div');
-    outer.style.position = "absolute";
-    outer.style.top = "0px";
-    outer.style.left = "0px";
-    outer.style.visibility = "hidden";
-    outer.style.width = "200px";
-    outer.style.height = "150px";
-    outer.style.overflow = "hidden";
-    outer.appendChild (inner);
+ var outer = document.createElement('div');
+ outer.style.position = "absolute";
+ outer.style.top = "0px";
+ outer.style.left = "0px";
+ outer.style.visibility = "hidden";
+ outer.style.width = "200px";
+ outer.style.height = "150px";
+ outer.style.overflow = "hidden";
+ outer.appendChild (inner);
 
-    document.body.appendChild (outer);
-    var w1 = inner.offsetWidth;
-    outer.style.overflow = 'scroll';
-    var w2 = inner.offsetWidth;
-    if (w1 == w2) w2 = outer.clientWidth;
+ document.body.appendChild (outer);
+ var w1 = inner.offsetWidth;
+ outer.style.overflow = 'scroll';
+ var w2 = inner.offsetWidth;
+ if (w1 == w2) w2 = outer.clientWidth;
 
-    document.body.removeChild (outer);
+ document.body.removeChild (outer);
 
-    return (w1 - w2);
-}*/
+ return (w1 - w2);
+ }*/
 
 // for example
 /*        var expandData = $(this).attr('data-expand');
 
-        if (expandData === 'true') {
-            $(this).attr('data-expand', 'false');
-        }
-        else if (expandData === 'false') {
-            $(this).attr('data-expand', 'true');
-        }*/
+ if (expandData === 'true') {
+ $(this).attr('data-expand', 'false');
+ }
+ else if (expandData === 'false') {
+ $(this).attr('data-expand', 'true');
+ }*/
 
-$(document).ready(function() {
-  //console.log("ready windows.height: ", $(window).height());
-  $("#main").height($(window).height() - 88 - 50);
+function setMainHeight() {
+    // console.log("ready windows.height: ", $(window).height());
+    // 88 = header + padding*2 (68+10*2)
+    // 60 = footer + padding*2 + border (30+10*2+10)
+    $("#main").height($(window).height() - 88 - 60);
+}
+
+$(document).ready(function () {
+    setMainHeight();
 });
 
-$(window).resize(function() {
-  //console.log("resize windows.height: ", $(window).height());
-  $("#main").height($(window).height() - 88 - 50);
+$(window).resize(function () {
+    setMainHeight();
 });
 
