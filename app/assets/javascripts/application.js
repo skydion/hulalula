c17d731a5a5ccfd6cf9d631704d798da542c4a7f
Taras Brodovych <skydion@ukr.net>
2014-09-16 17:29:22 +0300

    Different changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,55 +16,6 @@
 //= require turbolinks
 //= require_tree .
 
-/*
- $(document).ready(function() {
- // Just for demonstration purposes, change the contents/active state using jQuery
- $("#menu ul li a").click(function () {
- $("#menu ul li a").each(function () {
- $(this).removeClass("active");
- });
-
- $(this).addClass("active");
- });
- });
- */
-
-/*function getScrollBarWidth () {
- var inner = document.createElement('p');
- inner.style.width = "100%";
- inner.style.height = "200px";
-
- var outer = document.createElement('div');
- outer.style.position = "absolute";
- outer.style.top = "0px";
- outer.style.left = "0px";
- outer.style.visibility = "hidden";
- outer.style.width = "200px";
- outer.style.height = "150px";
- outer.style.overflow = "hidden";
- outer.appendChild (inner);
-
- document.body.appendChild (outer);
- var w1 = inner.offsetWidth;
- outer.style.overflow = 'scroll';
- var w2 = inner.offsetWidth;
- if (w1 == w2) w2 = outer.clientWidth;
-
- document.body.removeChild (outer);
-
- return (w1 - w2);
- }*/
-
-// for example
-/*        var expandData = $(this).attr('data-expand');
-
- if (expandData === 'true') {
- $(this).attr('data-expand', 'false');
- }
- else if (expandData === 'false') {
- $(this).attr('data-expand', 'true');
- }*/
-
 function setMainHeight() {
     // 88 = header + padding*2 (68+10*2)
     // 60 = footer + padding*2 + border (30+10*2+10)
