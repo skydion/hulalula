ba4cf40a5e25064703352537816076d59bda736d
Taras Brodovych <skydion@ukr.net>
2014-09-17 21:30:23 +0300

    Change some css element parameters over js and remove constants

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,12 +18,25 @@
 
 function setMainHeight() {
     // 88 = header + padding*2 (68+10*2)
+    var pheader = $("#page_header");
+    var ph = pheader.height() +
+        parseInt(pheader.css('padding-top')) +
+        parseInt(pheader.css('padding-bottom'));
+    //console.log('ph: ' + ph + 'px');
+
     // 60 = footer + padding*2 + border (30+10*2+10)
+    var pfooter = $("#page_footer");
+    var pf = pfooter.height() +
+        parseInt(pfooter.css('padding-top')) +
+        parseInt(pfooter.css('padding-bottom')) +
+        parseInt(pfooter.css('border-bottom-width'));
+    //console.log('pf: ' + pf + 'px');
+
     var wh = $(window).height();
-    //console.log("windows.height: ", wh);
 
     var m = $("#main");
-    m.height(wh - 88 - 60);
+    m.height(wh - ph - pf);
+    m.css('top', ph+'px');
 
     // 250 = comment header
     $(".comments_area").height(m.height() - 250);
